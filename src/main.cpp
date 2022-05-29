87f456e5cc3d4ea69b92bf18e419075163d28e8c
csl <3079625093@qq.com>
2022-05-29 13:04:29 +0800

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,21 @@
-// #define PAR_USE_COLOR
+#define PAR_USE_COLOR
 #include "progressbar.hpp"
 #include <thread>
 
 int main(int argc, char const *argv[]) {
   try {
-    ns_pbar::ProgressBar bar(5, ns_pbar::RED, ns_pbar::LIGHT_YELLOW);
+    ns_pbar::ProgressBar bar(15, ns_pbar::RED, ns_pbar::LIGHT_YELLOW);
     for (int i = 0; i != bar.taskCount(); ++i) {
       bar.setCurTask(i, "doing task " + std::to_string(i + 1));
-      // way 1
-      std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+
+      // way 1 [this way makes program faster]
+      std::this_thread::sleep_for(std::chrono::milliseconds(50));
       bar.lock();
       std::cout << "Hello, world" << std::endl;
       bar.unlock();
 
-      // way 2 [this way makes code clean]
-      std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+      // way 2 [this way makes code cleaner]
+      std::this_thread::sleep_for(std::chrono::milliseconds(50));
       bar.lockAndUnlock([&]() {
         std::cout << "cur Idx is " << i + 1 << std::endl;
       });
